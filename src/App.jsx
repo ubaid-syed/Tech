234d7eeaca0cb89d88d5febccf5cc04f71b46ced
ubaid-syed <arooj.ubaid777@gmail.com>
2024-09-09 22:42:33 +0500

    add contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import ContactPage from "./Screens/ContactPage/ContactPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/contact-page" element={<ContactPage />} />
       </Routes>
     </Router>
   );
